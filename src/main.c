6076b14656248f6367845beadd845fe3d7b4d445
gmmd <gmmdtnt@gmail.com>
2025-07-19 01:49:24 +0300

    Brick breaker Game

--- src/main.c
+++ src/main.c
@@ -1,28 +1,30 @@
 #include <ch32v00x.h>
-#include "utils.h"
-#include "twi.h"
-#include "ssd1306.h"
+
 #include "encoder.h"
 #include "gui.h"
+#include "ssd1306.h"
+#include "twi.h"
+#include "utils.h"
 
-static const char* const ITEMS[] = {
-    "Oscilloscope", "Voltage", "Snake game", "INA226"
-};
+static const char* const ITEMS[] = {"Oscilloscope", "Voltage", "Snake game",
+                                    "INA226", "Brick Breaker"};
 
 extern void oscilloscope(void);
 extern void voltage(void);
 extern void snake(void);
 extern void power(void);
+extern void brick_breaker(void);
 
 int main(void) {
     TWI_Init(400000);
 
-    Encoder_Init(); // To prevent phantom button click, initialize before delay
+    Encoder_Init();  // To prevent phantom button click, initialize before
+                     // delay
 
     delay_ms(50);
 
-    if (! ssd1306_begin()) {
-        while (1) {}
+    if (!ssd1306_begin()) {
+        while (1);
     }
 
     while (1) {
@@ -38,6 +40,9 @@ int main(void) {
                 break;
             case 3:
                 power();
+                break;
+            case 4:
+                brick_breaker();
                 break;
         }
     }
